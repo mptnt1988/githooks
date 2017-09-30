ec214827cf54a8ea6f994de26e7703ec98b7dd4a
Tuan Tran <mptnt1988@gmail.com>
2017-09-30 17:54:19 +0700

    Modify Python pre-push example

--- pre-push.py
+++ pre-push.py
@@ -5,5 +5,8 @@ import sys
 print(sys.argv)
 print(input().split())
 sys.stdin = open("/dev/tty", "r")
-X = input("Do you want to continue?")
-print("ok, done")
+X = input("Do you want to continue? (yes/...)")
+if X == "yes":  # Type 'yes' and enter
+    print("ok, done")
+else:
+    print("Not ok, ending...")
